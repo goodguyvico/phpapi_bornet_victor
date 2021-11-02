e6cdde54a1da73b0cc4a156f9ed9e068eb66e8d3
Victor Bornet <vbornet@stagiaire-humanbooster.com>
2021-11-02 12:53:43 +0100

    api

--- controllers/catList.php
+++ controllers/catList.php
@@ -3,5 +3,6 @@
 $response = file_get_contents('https://api.thecatapi.com/v1/breeds?limit=100&page=0');
 $cats = json_decode($response);
 
-var_dump($cats);
+// l'affichage n'est pas fini.
+
 include 'views/catListView.php';
